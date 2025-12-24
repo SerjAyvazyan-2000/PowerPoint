1daae40f74999e29e5e380832ac14ffcaf6401be
Serzh Ayvazyan <ayvazyanserj3@gmail.com>
2025-12-24 20:40:42 +0400

    Serzh Commit

---
+++ assets/js/swiper.js
@@ -0,0 +1,27 @@
+
+
+const reviewsSwiper = new Swiper(".reviews-swiper", {
+  spaceBetween: 10,
+  slidesPerView:1,
+    autoHeight: true,
+  loop:true,
+  pagination: {
+    el: ".reviews-swiper-pagination",
+    clickable: true,
+  },
+
+});
+
+
+
+const forWhomSwiper = new Swiper(".for-whom-swiper", {
+  spaceBetween: 10,
+  slidesPerView:1,
+    autoHeight: true,
+  loop:true,
+  pagination: {
+    el: ".for-whom-pagination",
+    clickable: true,
+  },
+
+});
